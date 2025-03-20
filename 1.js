63f74db9a61dd2696fec7abbb298060706096eec
FARUKHAHMED346 <fummer7788@gmail.com>
2025-03-20 10:29:01 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+var employee;
+([number, "string"]);
+employee([1, "FIFTH"], [2, "SEM"], [3, "CSE"]);
+console.log(employee);
+employee.push([4, "diploma"]);
+employee.push([5, "polytechic"]);
+console.log(employee);
+employee.pop();
+console.log(employee);
+employee.sort();
+console.log(employee);
